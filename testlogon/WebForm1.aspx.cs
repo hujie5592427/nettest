9f5f4ad8c8c1c8139ee1aa8cae683ad71aeaaafa
hujie5592427@163.com <308165757@qq.com>
2016-10-09 16:42:08 +0800

    aa

---
+++ testlogon/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace testlogon
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            string userName = Request["username"].ToString();
+            string password = Request["password"].ToString();
+            Response.Write("用户名：" + userName);
+            Response.Write("<br/>");
+            Response.Write("<br/>");
+            Response.Write("密码：" + password);
+            Response.Write("<br/>");
+
+            foreach (object obj in Request.ServerVariables)
+            {
+                string name = obj.ToString();
+                string value = Request.ServerVariables[name];
+                Response.Write(name + ":" + value + "<br/>");
+            }
+
+            //string user = Request.ServerVariables["LOGON_USER"].ToString();
+            //Response.Write(user);
+            Response.Write("<br/>");
+            Response.Write("end");
+
+            Response.Write(Environment.UserName + "<br/>");
+
+            Hashtable ht =(Hashtable) Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry de in ht) {
+                string s1 = de.Key.ToString();
+                string s2 = de.Value.ToString();
+                Response.Write(s1 + ":" + s2 + "<br/>");
+            }
+
+        }
+    }
+}
---
+++ testlogon/obj/Release/Package/PackageTmp/test.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="testlogon.test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form method="post" action="WebForm1.aspx">
+    <div>
+        <input id="text1" name="username" type="text"/><br />
+        <input id="text2" name="password" type="text"/><br />
+        <input id="login" type="submit" value="提交" /><br />
+    </div>
+    </form>
+</body>
+</html>
